50f7d16d4f96f800f04567a3bc0758719d36d0b9
JuanBatkis <juanbatkis@gmail.com>
2021-01-08 01:19:38 -0300

    Finished Login and Sign Up error notifications

--- src/containers/authContainer/AuthContainer.js
+++ src/containers/authContainer/AuthContainer.js
@@ -39,12 +39,19 @@ export default class AuthContainer extends Component {
 
             console.log('Congrats', response);
         }).catch((error) => {
-            console.log('An error occurred', error.response);
-            //almacenamos los errores una variable y sacamos solo los mensajes!!
-            const errors = Object.values(error.response.data.msg)
+            //console.log('An error occurred', error.response);
+            const data = error.response.data;
+            let msg = ''
+            if ('validationError' in data) {
+                msg = Object.values(data.validationError);
+                //console.log('msg 1: ', msg);
+            } else {
+                msg = Object.values(data);
+                //console.log('msg 2: ', msg);
+            }
             //recorremos el arreglo y constuimos la notificacion (importar buildNotification)
             //error = al mensaje, "danger" = al color de la notificacion, close = si tiene o no metodo para cerra
-            errors.map((error) => buildNotification(error,"danger","close"))
+            msg.map((msg) => buildNotification(msg,"danger","warning"))
         });
     }
 
--- src/utils/notification.js
+++ src/utils/notification.js
@@ -4,7 +4,7 @@ export const buildNotification = (
     message,
     status,
     icon = "check",
-    pos = "top-right"
+    pos = "top-right",
 ) => {
     UIkit.notification({
         message: `<span uk-icon='icon: ${icon}'></span> ${message}`,
